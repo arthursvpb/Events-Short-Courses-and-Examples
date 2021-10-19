c53d93acb8ad14dee99f71db396dfc3fde1d2418
arthursvpb <arthurandroid15@gmail.com>
2021-10-19 19:02:38 -0300

    adding express, typescript and prisma

---
+++ nlw-heat-07/backend/README.md
@@ -0,0 +1,10 @@
+# Node Heat Back-end
+
+## Prisma
+
+ORM que está sendo bastante difundida no mercado
+
+```
+yarn add prisma -D
+yarn prisma init
+```
---
+++ nlw-heat-07/backend/src/app.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`✨ Server is running on PORT ${PORT}`));
