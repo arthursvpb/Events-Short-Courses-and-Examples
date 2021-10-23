cac3eb797c0036b8cb05299378d2211757411fa2
arthursvpb <arthurandroid15@gmail.com>
2021-10-23 19:03:38 -0300

    add back-end integration

--- nlw-heat-07/web/src/components/MessageList/index.tsx
+++ nlw-heat-07/web/src/components/MessageList/index.tsx
@@ -1,61 +1,52 @@
+import { useState, useEffect } from 'react';
+
 import styles from './styles.module.scss';
 
 import logo from '../../assets/logo.svg';
 
+import { api } from '../../services/api';
+
+interface Message {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+    login: string;
+  };
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  async function fetchMessageList() {
+    const { data: fetchedMessages } = await api.get<Message[]>(
+      '/messages/last3'
+    );
+
+    setMessages(fetchedMessages);
+  }
+
+  useEffect(() => {
+    fetchMessageList();
+  }, []);
+
   return (
     <div className={styles.messsageListWrapper}>
       <img src={logo} alt='Do While 2021' />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            nam consequuntur adipisci assumenda nesciunt asperiores, quae ut in
-            at sit.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src='https://github.com/arthursvpb.png'
-                alt='Arthur Vasconcellos'
-              />
+        {messages.map(message => (
+          <li key={message.id} className={styles.message}>
+            <p className={styles.messageContent}>{message.text}</p>
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src={message.user.avatar_url} alt={message.user.name} />
+              </div>
+              <span>{message.user.login}</span>
             </div>
-            <span>Arthur Vasconcellos</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            nam consequuntur adipisci assumenda nesciunt asperiores, quae ut in
-            at sit.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src='https://github.com/arthursvpb.png'
-                alt='Arthur Vasconcellos'
-              />
-            </div>
-            <span>Arthur Vasconcellos</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            nam consequuntur adipisci assumenda nesciunt asperiores, quae ut in
-            at sit.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src='https://github.com/arthursvpb.png'
-                alt='Arthur Vasconcellos'
-              />
-            </div>
-            <span>Arthur Vasconcellos</span>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </div>
   );
